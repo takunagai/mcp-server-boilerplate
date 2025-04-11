c9dd37ecb1c7f225a66181282242839c66347552
takunagai <tak_na@icloud.com>
2025-04-12 04:28:15 +0900

    MCPサーバーの基本処理(与えられた数値を倍に)を実装

--- build/index.js
+++ build/index.js
@@ -1,12 +1,10 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
-
 const server = new McpServer({
-  name: "mcp-example",
-  version: "1.0.0",
+    name: "mcp-example",
+    version: "1.0.0",
 });
-
 /**
  * ツールの定義
  * nameとdescriptionを元に、実行するtoolが選択される
@@ -16,20 +14,13 @@ const server = new McpServer({
  * @param args - ツールの引数の定義
  * @param handler - ツールの実装
  */
-server.tool(
-  "double_number",
-  "与えられた数値を2倍にする",
-  {num: z.number().describe("数値")},
-  ({num}) => ({content: [{type: "text", text: (num * 2).toString()}]}),
-);
-
+server.tool("double_number", "与えられた数値を2倍にする", { num: z.number().describe("数値") }, ({ num }) => ({ content: [{ type: "text", text: (num * 2).toString() }] }));
 async function main() {
-  const transport = new StdioServerTransport();
-  await server.connect(transport);
-  console.error("Example MCP Server running on stdio");
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error("Example MCP Server running on stdio");
 }
-
 main().catch((error) => {
-  console.error("Fatal error in main():", error);
-  process.exit(1);
+    console.error("Fatal error in main():", error);
+    process.exit(1);
 });
